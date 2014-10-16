7e707d0def4996a7cbebbb2219ba545dfe7c498c
Allen Wang <wangyanxiang1024@gmail.com>
2014-10-16 16:45:57 +0800

    AtyNewVote layout changed

--- src/com/allenwang/vote/AtyNewVote.java
+++ src/com/allenwang/vote/AtyNewVote.java
@@ -2,15 +2,39 @@ package com.allenwang.vote;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AtyNewVote extends Activity {
+public class AtyNewVote extends Activity implements OnClickListener {
 
+	private Button mBtnCompleteSetup = null;
+	private Button mBtnDiscardVote = null;
+	private EditText mEtVoteName = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.aty_new_vote);
 		
+		mBtnCompleteSetup = (Button) findViewById(R.id.btnComplete);
+		mBtnDiscardVote = (Button) findViewById(R.id.btnDiscard);
 		
+		mBtnCompleteSetup.setOnClickListener(this);
+		mBtnDiscardVote.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.btnComplete:
+			
+			break;
+		case R.id.btnDiscard:
+			
+			break;
+		}
 	}
 	
 }
