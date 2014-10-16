1bfade27186edee76dc9d79432c59a921764766c
Allen Wang <wangyanxiang1024@gmail.com>
2014-10-16 18:49:47 +0800

    added AtyConfirmVote

---
+++ src/com/allenwang/vote/AtyConfirmVote.java
@@ -0,0 +1,14 @@
+package com.allenwang.vote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AtyConfirmVote extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.aty_confirm_vote);
+	}
+	
+}
--- src/com/allenwang/vote/AtyNewVote.java
+++ src/com/allenwang/vote/AtyNewVote.java
@@ -1,38 +1,53 @@
 package com.allenwang.vote;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class AtyNewVote extends Activity implements OnClickListener {
 
-	private Button mBtnCompleteSetup = null;
-	private Button mBtnDiscardVote = null;
+	private Button mBtnBack = null;
+	private Button mBtnNext = null;
 	private EditText mEtVoteName = null;
+	private ToggleButton mTbIfSecret = null;
+	
+	private String mVoteName = null;
+	private Boolean mIsToggleOn = false;
+	private String mVoteMode = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.aty_new_vote);
 		
-		mBtnCompleteSetup = (Button) findViewById(R.id.btnComplete);
-		mBtnDiscardVote = (Button) findViewById(R.id.btnDiscard);
+		mBtnBack = (Button) findViewById(R.id.btnBack);
+		mBtnNext = (Button) findViewById(R.id.btnNext);
+		mEtVoteName = (EditText) findViewById(R.id.etName);
+		mTbIfSecret = (ToggleButton) findViewById(R.id.tbIfSecret);
 		
-		mBtnCompleteSetup.setOnClickListener(this);
-		mBtnDiscardVote.setOnClickListener(this);
+		mBtnBack.setOnClickListener(this);
+		mBtnNext.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
 		switch(v.getId()) {
-		case R.id.btnComplete:
-			
+		case R.id.btnBack:
+			//不好,应该保存该界面的数据而不是直接关闭
+			finish();
 			break;
-		case R.id.btnDiscard:
-			
+		case R.id.btnNext:
+			mVoteName = mEtVoteName.getText().toString();
+			mIsToggleOn = mTbIfSecret.isChecked();
+			System.out.println(mIsToggleOn + "");
+			if(mIsToggleOn) mVoteMode = "记名投票"; else mVoteMode = "不记名投票";
+			Intent i = new Intent(AtyNewVote.this, AtyConfirmVote.class);
+			startActivity(i);
 			break;
 		}
 	}
